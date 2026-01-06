df996f0b09a552556b80370368c30aab6496b509
bnicenboim <bruno.nicenboim@gmail.com>
2026-01-06 06:32:52 +0100

    Replace Hugo site with Quarto site

---
+++ common.R
@@ -0,0 +1,47 @@
+library(stringr)
+# endcit<- "</cite></p>"
+endcit<- "</cite></p>"
+begcit<- "<p><cite>"
+
+output <- function(bibobj, withbrackets = TRUE, hack = FALSE){
+    for(i in 1:length(bibobj)){
+        a <- capture.output(print(bibobj[i], .opts = list(no.print.fields=c("eprint","URL") )))
+              if(hack){
+            #do ugly hack to remove the sentence between em, which is repeated in the case of the misc
+            a <- str_replace(a, "<em>(.*?)</em>\\.", "")
+              }
+        a <- paste(a, collapse = " ")
+        a <- substr(a,nchar(begcit)+1,nchar(a)) # remore the <cite>
+        a <- paste("<p>",a)
+        a <- substr(a,1,nchar(a)-nchar(endcit))
+        a <- str_replace(a, "(B\\. Nicenboim|Nicenboim, B\\.|Nicenboim, B)", "**\\1**")
+
+        if(withbrackets){
+          bibfile <-  paste0("bibs/",names(bibobj[i]), ".bib")
+                # My custom created fields:
+                brackets <- list(eprint= bibobj[i]$eprint,
+                                 read= bibobj[i]$url,
+                     `code/data`=  bibobj[i]$customb,
+                     poster = bibobj[i]$customc,
+                     talk = bibobj[i]$customd,
+                     bib = bibfile )
+                RefManageR::WriteBib(bibobj[i], file = bibfile, verbose = FALSE)
+                
+                # Adding urls in case of need inside the []
+                for(b in 1:length(brackets)){
+                    if(!is.null(brackets[[b]])){
+                        if(str_starts(brackets[[b]], "http") | str_ends(brackets[[b]], "pdf|bib" )){
+                            a <- paste0(a,' **[**<a href="',brackets[[b]],'">**',names(brackets[b]),'**</a>**]** ')    
+                        } else {  #This is mostly to create the [ talk ] with no link
+                            a <- paste0(a,' **[**',brackets[b],'**]** ')
+                        }
+                        
+                    }
+                }
+                }
+        a <-paste0(a,"</p>")  #without the </cite>
+        cat(a)
+    }
+
+}
+
